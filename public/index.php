6c283aecdcdbd1a1eeb80f6ba7ca8c2a7c521469
karimsamir <karim5977@gmail.com>
2016-04-20 13:48:21 +0200

    rewrite rule

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+
+echo "Request URL = '" . $_SERVER["QUERY_STRING"] . "'";
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
