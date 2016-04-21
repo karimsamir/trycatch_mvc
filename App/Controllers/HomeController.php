b7a2b4560495abd8ce3aec097a1142164a99cbdc
karimsamir <karim5977@gmail.com>
2016-04-21 20:40:54 +0200

    add new controller for testing

---
+++ App/Controllers/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace App\Controllers;
+
+/**
+ * Description of PostController
+ *
+ * @author karim
+ */
+class HomeController {
+
+    //put your code here
+
+    public function index() {
+        echo 'Hello world from Home controller and index action';
+    }
+}
