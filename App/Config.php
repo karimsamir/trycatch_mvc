64840394ce34ed0a64bb6a1562153f4007b1fd03
karimsamir <karim5977@gmail.com>
2016-04-24 21:17:31 +0200

    adding a config file to handle database connection string for now

---
+++ App/Config.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace App;
+
+/* * Application Config file
+ *
+ * @author karim
+ */
+
+class Config {
+
+// Database connection
+    const DB_HOST = "localhost";
+    const DB_NAME = "mvc";
+    const DB_USER = "root";
+    const DB_PASSWORD = "";
+
+}
