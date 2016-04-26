ee61645a03b162198550d8f346e517256d6f41d4
karimsamir <karim5977@gmail.com>
2016-04-26 14:30:05 +0200

    display contacts in table

--- App/Views/address/index.twig.php
+++ App/Views/address/index.twig.php
@@ -1,20 +1,44 @@
 {% extends "base.html" %}
 
-    {% block title %} 
-        All Contacts
-    {% endblock %}
+{% block title %} 
+All Contacts
+{% endblock %}
 
 {% block body %} 
-    <h1>Contacts</h1>
+<h1>Contacts</h1>
 
-    <ul>
-        {% for contact in contacts %}
-        <li>
-            <h2>{{ contact.name }}</h2>
-            <p>Phone: {{ contact.phone }}</p>
-            <p>Address: {{ contact.address }}</p>
-        </li>
-        {% endfor %}
-    </ul>
-    
+
+<table class="table table-striped table-bordered">
+    <tr><th>Name</th>
+        <th>Phone</th>
+        <th>Address</th>
+        <th></th>
+    </tr>
+    {% for contact in contacts %}
+    <tr>
+        <td>{{contact.name}}</td>
+        <td>{{contact.phone}}</td>
+        <td>{{contact.address}}</td>
+        <td>
+            <button class="btn btn-xs btn-primary">
+                Edit
+            </button>
+            <button class="btn btn-xs btn-danger">
+                Delete
+            </button>
+        </td>
+    </tr>
+    {% endfor %}
+</table>
+
+<!--<ul>
+    {% for contact in contacts %}
+    <li>
+        <h2>{{ contact.name }}</h2>
+        <p>Phone: {{ contact.phone }}</p>
+        <p>Address: {{ contact.address }}</p>
+    </li>
+    {% endfor %}
+</ul>-->
+
 {% endblock %}
